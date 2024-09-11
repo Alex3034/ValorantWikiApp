90a6b83a3bf5d43af0e4d80ef6cd7be37b1d2944
Alejandro.H <alex1348wk@gmail.com>
2024-09-11 18:52:36 +0200

    Dependencias
    Dependencias añadidas

--- app/src/main/java/com/valorantwiki/valorantwikiapp/data/api/AgentsService.kt
+++ app/src/main/java/com/valorantwiki/valorantwikiapp/data/api/AgentsService.kt
@@ -3,7 +3,7 @@ package com.valorantwiki.valorantwikiapp.data.api
 import com.valorantwiki.valorantwikiapp.data.model.Agent
 import retrofit2.http.GET
 
-interface ValorantApi {
+interface AgentsService {
     @GET("v1/agents")
     suspend fun getAgents(): AgentResponse
 }
