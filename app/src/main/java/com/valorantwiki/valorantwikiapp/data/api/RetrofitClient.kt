caf6725a25d7858f0db23d54399494185efb21b4
Alejandro.H <alex1348wk@gmail.com>
2024-09-11 18:32:32 +0200

    Reapply "Cliente"
    This reverts commit 476947c951f63f8aac608753f214a747a0279899.

--- app/src/main/java/com/valorantwiki/valorantwikiapp/data/api/RetrofitClient.kt
+++ app/src/main/java/com/valorantwiki/valorantwikiapp/data/api/RetrofitClient.kt
@@ -1,17 +1,22 @@
 package com.valorantwiki.valorantwikiapp.data.api
 
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.create
 
 object RetrofitClient {
 
     private const val BASE_URL = "https://valorant-api.com/"
+    private val json = Json { ignoreUnknownKeys = true }
 
-    val instance: ValorantApi by lazy {
+    val instance: AgentsService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
             .build()
-            .create(ValorantApi::class.java)
+            .create<AgentsService>()
     }
 }
