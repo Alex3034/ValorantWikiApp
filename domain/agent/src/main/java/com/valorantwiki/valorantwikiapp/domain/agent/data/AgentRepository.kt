cdf493d1988fadbf2b3516e3bb5f7864760a339a
Alejandro.H <alex1348wk@gmail.com>
2024-11-04 21:13:36 +0100

    fix versions

--- domain/agent/src/main/java/com/valorantwiki/valorantwikiapp/domain/agent/data/AgentRepository.kt
+++ domain/agent/src/main/java/com/valorantwiki/valorantwikiapp/domain/agent/data/AgentRepository.kt
@@ -5,18 +5,25 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.onEach
 
+interface IAgentRepository {
+    val agents: Flow<List<Agent>>
+    fun findAgentById(uuid: String): Flow<Agent>
+
+    suspend fun toggleFavorite(agent: Agent)
+}
+
 class AgentRepository(
     private val localDataSource: AgentLocalDataSource,
     private val remoteDataSource: AgentsRemoteDataSource
-) {
-    val agents: Flow<List<Agent>> = localDataSource.agents.onEach { localAgents ->
+) : IAgentRepository {
+    override val agents: Flow<List<Agent>> = localDataSource.agents.onEach { localAgents ->
         if (localAgents.isEmpty()) {
             val remoteAgents = remoteDataSource.fetchAgents()
             localDataSource.save(remoteAgents)
         }
     }
 
-    fun findAgentById(uuid: String): Flow<Agent> = localDataSource.getAgentById(uuid)
+    override fun findAgentById(uuid: String): Flow<Agent> = localDataSource.getAgentById(uuid)
         .onEach { agent ->
             if (agent == null) {
                 val remoteAgent = remoteDataSource.findAgentById(uuid)
@@ -25,7 +32,7 @@ class AgentRepository(
         }
         .filterNotNull()
 
-    suspend fun toggleFavorite(agent: Agent) {
+    override suspend fun toggleFavorite(agent: Agent) {
         localDataSource.save(listOf(agent.copy(isFavorite = !agent.isFavorite)))
     }
 }
