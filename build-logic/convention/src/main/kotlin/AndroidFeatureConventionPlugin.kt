91dc689d1af178d227a83c8ffd374ac9491fe044
Alejandro.H <alex1348wk@gmail.com>
2024-12-09 19:01:30 +0100

    add ui test dependences for feature plugin

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -16,6 +16,14 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 add("implementation", libs.findLibrary("androidx.lifecycle.viewmodel.compose").get())
                 add("testImplementation", libs.findLibrary("kotlinx.coroutines.test").get())
                 add("testImplementation", libs.findLibrary("turbine").get())
+                add("androidTestImplementation", project(":test:unit"))
+                add("androidTestImplementation", libs.findLibrary("androidx.compose.bom").get())
+                add("androidTestImplementation", libs.findLibrary("androidx.junit").get())
+                add("androidTestImplementation", libs.findLibrary("androidx.espresso.core").get())
+                add("androidTestImplementation", libs.findLibrary("androidx.ui.test.junit4").get())
+                add("androidTestImplementation", libs.findLibrary("androidx.test.core").get())
+                add("androidTestImplementation", libs.findLibrary("androidx.test.rules").get())
+                add("debugImplementation", libs.findLibrary("androidx.ui.test.manifest").get())
             }
         }
     }
